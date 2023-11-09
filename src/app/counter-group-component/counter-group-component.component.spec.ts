9ce3016fc9a1101ab64cfed05df5226016442266
Tianwei Liu <TLiu25@slb.com>
2023-11-09 21:05:44 +0800

    test: countergroup remove counter

--- src/app/counter-group-component/counter-group-component.component.spec.ts
+++ src/app/counter-group-component/counter-group-component.component.spec.ts
@@ -25,4 +25,11 @@ describe('CounterGroupComponentComponent', () => {
     component.onAdd();
     expect(component.counters.length).toEqual(initialLength + 1);
   });
+  it('should remove a counter', () => {
+    component.onAdd();
+    const initialLength = component.counters.length;
+    component.onRemove(0);
+    expect(component.counters.length).toEqual(initialLength - 1);
+  });
+
 });
